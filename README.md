9967932adec7aedcefb2f8bb9a8a200c74260364
Shao Cheng <astrohavoc@gmail.com>
2017-11-06 18:05:39 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# direct-rocksdb
+
+[![AppVeyor](https://ci.appveyor.com/api/projects/status/github/TerrorJack/direct-rocksdb?branch=master&svg=true)](https://ci.appveyor.com/project/TerrorJack/direct-rocksdb?branch=master)
---
+++ Setup.hs
@@ -0,0 +1,51 @@
+{-# OPTIONS_GHC -Wall -Wno-deprecations #-}
+
+import Data.Foldable
+import Distribution.Simple
+import Distribution.Simple.LocalBuildInfo
+import Distribution.Simple.Toolkit
+import Distribution.Types.BuildInfo
+import Distribution.Types.GenericPackageDescription
+import Distribution.Types.PackageDescription
+import System.Directory
+import System.FilePath
+
+main :: IO ()
+main =
+  defaultMainWithHooks
+    simpleUserHooks
+    { hookedPrograms = [cmakeProgram, ninjaProgram]
+    , confHook =
+        \t f -> do
+          lbi <- confHook simpleUserHooks t f
+          rocksdb_srcdir <- (</> "rocksdb-5.8") <$> getCurrentDirectory
+          let clbi = getComponentLocalBuildInfo lbi CLibName
+              rocksdb_libname =
+                getHSLibraryName (componentUnitId clbi) ++ "-rocksdb-5.8"
+              rocksdb_builddir = componentBuildDir lbi clbi </> "rocksdb-5.8"
+              lib_installdir =
+                libdir $
+                getComponentInstallDirs
+                  (packageDescription $ fst t)
+                  lbi
+                  CLibName
+          for_ [rocksdb_builddir, lib_installdir] $
+            createDirectoryIfMissing True
+          withCurrentDirectory rocksdb_builddir $ do
+            runLBIProgram lbi cmakeProgram [rocksdb_srcdir, "-G", "Ninja"]
+            runLBIProgram lbi ninjaProgram ["librocksdb.a"]
+            copyFile "librocksdb.a" $
+              lib_installdir </> "lib" ++ rocksdb_libname <.> "a"
+          pure
+            lbi
+            { localPkgDescr =
+                updatePackageDescription
+                  ( Just
+                      emptyBuildInfo
+                      { extraLibs = [rocksdb_libname]
+                      , extraLibDirs = [lib_installdir]
+                      }
+                  , []) $
+                localPkgDescr lbi
+            }
+    }

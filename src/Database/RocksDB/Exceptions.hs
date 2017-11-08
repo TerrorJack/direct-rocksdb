6f06baccb064f5a17ac11307c875af839769d602
Shao Cheng <astrohavoc@gmail.com>
2017-11-08 17:49:27 +0800

    Do not expose functions in Utils module

--- src/Database/RocksDB/Exceptions.hs
+++ src/Database/RocksDB/Exceptions.hs
@@ -2,26 +2,11 @@
 
 module Database.RocksDB.Exceptions
   ( RocksDBException(..)
-  , withErrorMessagePtr
   ) where
 
 import Control.Exception.Safe
 import qualified Data.ByteString as BS
-import Foreign
-import Foreign.C
 
 newtype RocksDBException = RocksDBException
   { errorMessage :: BS.ByteString
   } deriving (Show, Exception)
-
-withErrorMessagePtr :: (Ptr (Ptr CChar) -> IO r) -> IO r
-{-# INLINEABLE withErrorMessagePtr #-}
-withErrorMessagePtr cont =
-  alloca $ \p -> do
-    r <- cont p
-    p' <- peek p
-    if p' == nullPtr
-      then pure r
-      else do
-        msg <- BS.packCString p'
-        throw $ RocksDBException msg
--- src/Database/RocksDB/Utils.hs
+++ src/Database/RocksDB/Utils.hs
@@ -1,9 +1,14 @@
 module Database.RocksDB.Utils
   ( setIntOptions
   , setBoolOptions
+  , withErrorMessagePtr
   ) where
 
+import Control.Exception.Safe
+import qualified Data.ByteString as BS
 import Data.Foldable
+import Foreign
+import Foreign.C
 
 setIntOptions :: Integral t => Maybe Int -> (t -> IO ()) -> IO ()
 {-# INLINEABLE setIntOptions #-}
@@ -17,3 +22,15 @@ setBoolOptions field cont =
     if val
       then 1
       else 0
+
+withErrorMessagePtr :: (Ptr (Ptr CChar) -> IO r) -> IO r
+{-# INLINEABLE withErrorMessagePtr #-}
+withErrorMessagePtr cont =
+  alloca $ \p -> do
+    r <- cont p
+    p' <- peek p
+    if p' == nullPtr
+      then pure r
+      else do
+        msg <- BS.packCString p'
+        throw $ RocksDBException msg

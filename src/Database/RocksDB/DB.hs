1f96446b6bfab12e8c0316e1270a35a4c08e6326
Shao Cheng <astrohavoc@gmail.com>
2017-11-08 17:33:34 +0800

    Basic put/get is supposed to work by now

--- src/Database/RocksDB/DB.hs
+++ src/Database/RocksDB/DB.hs
@@ -1,7 +1,13 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module Database.RocksDB.DB
   ( openDB
+  , putDB
+  , getDB
   ) where
 
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Unsafe as BS
 import Database.RocksDB.Exceptions
 import Database.RocksDB.Internals
 import Database.RocksDB.Options
@@ -17,3 +23,41 @@ openDB opts path = do
     withCString path $ \path_ptr -> do
       db_ptr <- withErrorMessagePtr $ c_rocksdb_open opts_ptr path_ptr
       newConcForeignPtr db_ptr $ c_rocksdb_close db_ptr
+
+putDB ::
+     ForeignPtr Rocksdb
+  -> ForeignPtr RocksdbWriteoptions
+  -> BS.ByteString
+  -> BS.ByteString
+  -> IO ()
+{-# INLINEABLE putDB #-}
+putDB db_fptr wopts_fptr k_bs v_bs =
+  withForeignPtr db_fptr $ \db_ptr ->
+    withForeignPtr wopts_fptr $ \wopts_ptr ->
+      BS.unsafeUseAsCStringLen k_bs $ \(k_buf, k_len) ->
+        BS.unsafeUseAsCStringLen v_bs $ \(v_buf, v_len) ->
+          withErrorMessagePtr $
+          c_rocksdb_put
+            db_ptr
+            wopts_ptr
+            k_buf
+            (fromIntegral k_len)
+            v_buf
+            (fromIntegral v_len)
+
+getDB ::
+     ForeignPtr Rocksdb
+  -> ForeignPtr RocksdbReadoptions
+  -> BS.ByteString
+  -> IO BS.ByteString
+{-# INLINEABLE getDB #-}
+getDB db_fptr ropts_fptr k_bs =
+  withForeignPtr db_fptr $ \db_ptr ->
+    withForeignPtr ropts_fptr $ \ropts_ptr ->
+      BS.unsafeUseAsCStringLen k_bs $ \(k_buf, k_len) ->
+        alloca $ \(v_len_buf :: Ptr CSize) -> do
+          v_buf <-
+            withErrorMessagePtr $
+            c_rocksdb_get db_ptr ropts_ptr k_buf (fromIntegral k_len) v_len_buf
+          v_len <- fromIntegral <$> peek v_len_buf
+          BS.unsafePackMallocCStringLen (v_buf, v_len)
--- test/direct-rocksdb-test.hs
+++ test/direct-rocksdb-test.hs
@@ -1,6 +1,10 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 import Control.Exception.Safe
 import Database.RocksDB.DB
 import Database.RocksDB.Options
+import Database.RocksDB.ReadOptions
+import Database.RocksDB.WriteOptions
 import Foreign
 import System.Directory
 import System.FilePath
@@ -10,4 +14,10 @@ main = do
   tmp_dir <- getTemporaryDirectory
   let opts = defaultOptions {createIfMissing = Just True}
       db_path = tmp_dir </> "direct-rocksdb-test.rocksdb"
-  bracket (openDB opts db_path) finalizeForeignPtr print
+  bracket (openDB opts db_path) finalizeForeignPtr $ \db_fptr ->
+    bracket (marshalWriteOptions defaultWriteOptions) finalizeForeignPtr $ \wopts_fptr -> do
+      putDB db_fptr wopts_fptr "key" "value"
+      bracket (marshalReadOptions defaultReadOptions) finalizeForeignPtr $ \ropts_fptr -> do
+        v <- getDB db_fptr ropts_fptr "key"
+        print v
+        assert (v == "value") $ pure ()

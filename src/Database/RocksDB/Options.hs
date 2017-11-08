1c1564bdbbe7e2b3ccf182ea23ca2fbf0752e8f2
Shao Cheng <astrohavoc@gmail.com>
2017-11-08 11:32:27 +0800

    Begin implementing higher level bindings

---
+++ src/Database/RocksDB/Options.hs
@@ -0,0 +1,31 @@
+{-# LANGUAGE RecordWildCards #-}
+
+module Database.RocksDB.Options
+  ( Options(..)
+  , defaultOptions
+  , marshalOptions
+  ) where
+
+import Data.Foldable
+import Database.RocksDB.Internals
+import Foreign
+
+data Options = Options
+  { totalThreads :: !(Maybe Int)
+  , createIfMissing :: !(Maybe Bool)
+  }
+
+defaultOptions :: Options
+defaultOptions = Options {totalThreads = Nothing, createIfMissing = Nothing}
+
+marshalOptions :: Options -> IO (IO (), Ptr RocksdbOptions)
+marshalOptions Options {..} = do
+  opts_p <- c_rocksdb_options_create
+  for_ totalThreads $ \total_threads ->
+    c_rocksdb_options_increase_parallelism opts_p $ fromIntegral total_threads
+  for_ createIfMissing $ \flag ->
+    c_rocksdb_options_set_create_if_missing opts_p $
+    if flag
+      then 1
+      else 0
+  pure (c_rocksdb_options_destroy opts_p, opts_p)
